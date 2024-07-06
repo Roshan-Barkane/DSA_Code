d2e9974264dde4b457cc0692326af73924bc3daa
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-07-06 06:27:49 +0530

    write the simple structure in c++ and without using namespane

--- c++.cpp
+++ c++.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 int main()
 {
+    // \n and endl are word on similar
+    std ::cout << "Hey Roshan \n";
+    // add multiple Statement in single line
+
+    std ::cout << "Hey Roshan" << std::endl
+               << "chal nikal life se" << std::endl;
+    std ::cout << "Hey Roshan";
     return 0;
 }
